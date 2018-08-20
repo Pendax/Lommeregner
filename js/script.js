e5bdc745a612b988c618986a4069898c1ee57571
Pendax <frederik-lai@outlook.com>
2018-08-20 12:17:57 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+//'use strict';
+var slutdata = document.getElementById("resultat");
+var udregning = this.className;
+var bestemmer = this.id;
+function buttonclick() {
+    switch (udregning) {
+        case instruktion:
+            switch (document.getElementById()) {
+                case plus:
+                    slutdata.innerHTML = slutdata.value + " " + "+"
+                    break;
+                case minus:
+                    break;
+            }
+        break;
+        case nummer:
+        break;
+    }
+    return;
+}
